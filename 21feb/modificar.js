d2d50ce7949a000799e00ff7d0903d9041bc5d54
Alexa17120676 <61323989+Alexa17120676@users.noreply.github.com>
2020-03-03 13:52:03 -0600

    Add files via upload

---
+++ 21feb/modificar.js
@@ -0,0 +1,24 @@
+function dark(){
+	
+	
+	document.getElementById("cabeza").id = "cabezaDark";
+	document.getElementById("buscar").id = "buscarDark";
+    document.getElementById("all").id="allDark";
+    document.getElementById("cuenta").id = "cuentaDark";
+	document.getElementById("texto").id = "textoDark";
+    document.getElementById("footer").id = "footerDark";
+    document.getElementById("boton1").id = "boton1Dark";
+    document.getElementById("boton2").id = "boton2Dark";
+}
+
+function normal(){
+		document.getElementById("cabezaDark").id = "cabeza";
+		document.getElementById("buscarDark").id= "buscar";
+		document.getElementById("allDark").id = "all";
+        document.getElementById("textoDark").id = "texto";
+        document.getElementById("footerDark").id = "footer";
+        document.getElementById("cuentaDark").id = "cuenta";
+        document.getElementById("boton1Dark").id = "boton1";
+        document.getElementById("boton2Dark").id = "boton2";
+	/*	document.getElementsByClassName("botonDark").className="boton";*/
+	}
